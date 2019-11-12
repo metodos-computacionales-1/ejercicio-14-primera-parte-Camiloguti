ffd75f6d9bef2c4a4bd3014cb8312881bd131a62
Camiloguti <53878805+Camiloguti@users.noreply.github.com>
2019-11-12 16:59:09 -0500

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,139 @@
+//Codigo de presentacion
+#include <iostream>
+#include <cmath>
+using namespace std;
+double f0(double t, double y0, double y1); //Returns derivative of y0
+double f1(double t, double y0, double y1); //Returns derivative of y1
+
+const double K = 100;
+const double M = 2;
+const double LAMBDA = 1;
+const double DeltaT = 0.01;
+
+int main(void)
+{
+    return 0;
+}
+
+double f0(double t, double y0, double y1)
+{
+  return y1;
+}
+
+double f1(double t, double y0, double y1)
+{
+  return (-K/M)*pow(y0, LAMBDA);
+}
+
+void rk4(double t, double h, double & y0, double & y1) // metodo de runge kutta 4 orden
+{
+  double k10, k11, k20, k21, k30, k31, k40, k41;
+  k10 = h*f0(t, y0, y1);
+  k11 = h*f1(t, y0, y1);
+  k20 = h*f0(t+h/2, y0 + k10/2, y1 + k11/2);
+  k21 = h*f1(t+h/2, y0 + k10/2, y1 + k11/2);
+  k30 = h*f0(t+h/2, y0 + k20/2, y1 + k21/2);
+  k31 = h*f1(t+h/2, y0 + k20/2, y1 + k21/2);
+  k40 = h*f0(t + h, y0 + k30, y1 + k31);
+  k41 = h*f1(t + h, y0 + k30, y1 + k31);
+
+  y0 = y0 + (1.0/6.0)*(k10 + 2*k20 + 2*k30 + k40);
+  y1 = y1 + (1.0/6.0)*(k11 + 2*k21 + 2*k31 + k41);
+}
+Implementation of the whole code link
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+// variable constantes globales
+const double K = 100;
+const double M = 2;
+const double LAMBDA = 1;
+const double DeltaT = 0.01;
+
+// declaracion de funciones
+double f0(double t, double y0, double y1); // derivada de y0
+double f1(double t, double y0, double y1); // derivada de y1
+void rk4(double t, double h, double & y0, double & y1); // metodo de runge kutta 4 orden
+
+
+{
+  double x, v, time;
+  x = 1;
+  v = 0;
+  for(time = 0; time <= 10; time += DeltaT) {
+    cout << time << "\t" << x << "\t" << v << endl;
+    rk4(time, DeltaT, x, v);
+  }
+
+
+  return 0;
+}
+double f0(double t, double y0, double y1)
+{
+  return y1;
+}
+
+double f1(double t, double y0, double y1)
+{
+  return (-K/M)*pow(y0, LAMBDA);
+}
+
+void rk4(double t, double h, double & y0, double & y1) // metodo de runge kutta 4 orden
+{
+  double k10, k11, k20, k21, k30, k31, k40, k41;
+  k10 = h*f0(t, y0, y1);
+  k11 = h*f1(t, y0, y1);
+  k20 = h*f0(t+h/2, y0 + k10/2, y1 + k11/2);
+  k21 = h*f1(t+h/2, y0 + k10/2, y1 + k11/2);
+  k30 = h*f0(t+h/2, y0 + k20/2, y1 + k21/2);
+  k31 = h*f1(t+h/2, y0 + k20/2, y1 + k21/2);
+  k40 = h*f0(t + h, y0 + k30, y1 + k31);
+  k41 = h*f1(t + h, y0 + k30, y1 + k31);
+
+  y0 = y0 + (1.0/6.0)*(k10 + 2*k20 + 2*k30 + k40);
+  y1 = y1 + (1.0/6.0)*(k11 + 2*k21 + 2*k31 + k41);
+}
+Note
+Here we solved the problem for two variables, however, from time to time we have to deal with problems that involves many variables therefore the implementation by hand can be quite annoying, so here i implemented a code in c++ that can handle N different variables. I hope it can be useful for the future, take a look at it.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+using namespace std;
+
+// variable constantes globales
+const double K = 100;
+const double M = 2;
+const double LAMBDA = 1;
+const double DeltaT = 0.01;
+
+// declaracion de funciones
+double f0(double t, double y0, double y1); // derivada de y0
+double f1(double t, double y0, double y1); // derivada de y1
+void rk4(double t, double h, double & y0, double & y1); // metodo de runge kutta 4 orden
+
+
+{
+  double x, v, time;
+  x = 1;
+  v = 0;
+  for(time = 0; time <= 10; time += DeltaT) {
+    cout << time << "\t" << x << "\t" << v << endl;
+    rk4(time, DeltaT, x, v);
+  }
+
+
+  return 0;
+}
